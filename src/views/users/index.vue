fb13e55b738300387377efd0589385ab10c96703
View <1140182402@qq.com>
2022-09-17 16:23:09 +0800

    u

---
+++ src/views/users/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 style="text-align: center;margin-top: 100px">专利检测</h1>
+    <img style="width: 80%;margin: 0 10%" src="https://www.helloimg.com/images/2022/09/16/Z6LLbt.png">
+  </div>
+</template>
+<script>
+export default {
+  name: 'Users',
+  data() {
+    return {
+      currentRole: 'adminDashboard'
+    }
+  },
+  created() {
+    if (!this.roles.includes('admin')) {
+      this.currentRole = 'editorDashboard'
+    }
+  }
+}
+</script>
